c9f8baae56b36840b0ab1d3fea13f43298139576
guigs44 <guigs45@outlook.com>
2022-08-26 20:06:21 -0300

    Added the base TextBoxModel Changed the UI again

---
+++ Engine/Models/TextBoxModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Engine.Models
+{
+	public class TextBoxModel
+	{
+		public int TabIndex { get; set; }
+		public string? Content { get; set; }
+		public bool IsInternal { get; set; } = true;
+	}
+}

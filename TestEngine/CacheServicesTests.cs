5e2ee4e9865514274fe0d130ae91e21ee22fb22b
guigs44 <guigs45@outlook.com>
2022-09-01 15:34:21 -0300

    Added xUnit for unit Testing

---
+++ TestEngine/CacheServicesTests.cs
@@ -0,0 +1,10 @@
+﻿namespace TestEngine
+{
+    public class CacheServicesTests
+    {
+        public void SaveTextBoxData_ShouldBeSuccessful()
+        {
+
+        }
+    }
+}
---
+++ TestEngine/DataSessionTests.cs
@@ -0,0 +1,18 @@
+﻿using Engine.ViewModels;
+using Xunit;
+
+namespace TestEngine
+{
+    public class DataSessionTests
+    {
+        [Fact]
+        public void DataSession_ShouldHaveAValidTabCollection()
+        {
+            var DataSession = new DataSession();
+            DataSession.AddEmptyTab(0);
+            Assert.True(DataSession.OpenTabs.Any());
+        }
+
+        
+    }
+}
